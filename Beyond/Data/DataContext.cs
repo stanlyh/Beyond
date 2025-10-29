1398274423d5e95f9ee0fc15846126926243e700
stanlyh <eduardostanly@gmail.com>
2025-10-28 23:03:07 -0500

    firts steps

--- Beyond/Data/DataContext.cs
+++ Beyond/Data/DataContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Beyond.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
+        public DbSet<Client> Clients => Set<Client>();
     }
 }
